fe190b62068eda225f08411ec5096a162facf8ca
RishabhPathak16 <rishabhpathak729@gmail.com>
2025-04-22 14:12:21 +0530

    java

---
+++ arr2.java
@@ -0,0 +1,15 @@
+public class arr2 {
+    public static void main(String[] args) {
+        int[] marks = new int[3];
+        marks[0] = 97; //physics
+        marks[1] = 98; //english
+        marks[2] = 99; //chemistry
+        
+        for(int i=0;i<3;i++){
+
+            System.out.println(marks[i]);
+
+        }
+    }
+    
+}
