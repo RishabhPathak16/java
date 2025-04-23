a0e41372ad67d6a60f038eb713959b49824936d4
RishabhPathak16 <rishabhpathak729@gmail.com>
2025-04-24 02:40:44 +0530

    java

---
+++ operators2.java
@@ -0,0 +1,9 @@
+public class operators2 {
+    public static void main(String[] args) {
+        int x,y,z;
+        x=15;
+        y=15;
+        z=x*y;
+        System.out.println(z);
+    }
+}
---
+++ operators3.java
@@ -0,0 +1,16 @@
+public class operators3 {
+    public static void main(String[] args) {
+         int x = 15;
+         x*=5;
+        System.out.println(x);
+
+        int y = 15;
+        y-=5;
+        System.out.println(y);
+
+         int z = 6;
+         z%=5;
+        System.out.println(z);
+
+     }
+}
