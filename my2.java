4f9604175e394d53d075b5d7ce43a6893224b694
RishabhPathak16 <rishabhpathak729@gmail.com>
2025-04-24 01:48:49 +0530

    java

---
+++ my2.java
@@ -0,0 +1,13 @@
+public class my2 {
+    public static void main(String[] args) {
+        int length = 15;
+        int width = 20;
+        int area;
+        
+        area = length*width;
+
+        System.out.println("length: "+ length);
+        System.out.println("width: " + width);
+        System.out.println("area of rectagnle: "+ area);
+    }
+}
---
+++ operators.java
@@ -0,0 +1,10 @@
+public class operators {
+    public static void main (String args[]){
+        
+        int x = 15;
+        int y = 15;
+        int z = x+y;
+        int p = z + 20;
+        System.out.println(p);
+    }
+}
